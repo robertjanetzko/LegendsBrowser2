9d4af37c222db7f68907a377bd24b41eeafe3846
Robert Janetzko <robert.janetzko@nhochdrei.de>
2022-04-15 14:46:51 +0000

    embed frontend

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,5 @@ go 1.18
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/iancoleman/strcase v0.2.0
 	github.com/pkg/profile v1.6.0
 )
--- backend/main.go
+++ backend/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"embed"
 	"flag"
 	"fmt"
+	"io/fs"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
@@ -16,9 +18,14 @@ import (
 
 var world *model.DfWorld
 
+//go:embed resources/frontend
+var frontend embed.FS
+
 func main() {
 	f := flag.String("f", "", "open a file")
 	flag.Parse()
+
+	router := mux.NewRouter().StrictSlash(true)
 
 	if len(*f) > 0 {
 		defer profile.Start(profile.MemProfile).Stop()
@@ -55,8 +62,6 @@ func main() {
 		// model.ListOtherElements("poeticForm", &world.PoeticForms)
 		// model.ListOtherElements("written", &world.WrittenContents)
 
-		router := mux.NewRouter().StrictSlash(true)
-
 		server.RegisterResource(router, "region", world.Regions)
 		// server.RegisterResource(router, "undergroundRegion", world.UndergroundRegions)
 		server.RegisterResource(router, "landmass", world.Landmasses)
@@ -72,12 +77,56 @@ func main() {
 		server.RegisterResource(router, "musicalForm", world.MusicalForms)
 		server.RegisterResource(router, "poeticForm", world.PoeticForms)
 		// server.RegisterResource(router, "written", world.WrittenContents)
-
-		spa := server.SpaHandler{StaticPath: "frontend/dist/legendsbrowser", IndexPath: "index.html"}
-		router.PathPrefix("/").Handler(spa)
-
-		fmt.Println("Serving at :8080")
-		http.ListenAndServe(":8080", router)
 	}
 
+	spa := spaHandler{staticFS: frontend, staticPath: "resources/frontend", indexPath: "index.html"}
+	router.PathPrefix("/").Handler(spa)
+
+	fmt.Println("Serving at :8080")
+	http.ListenAndServe(":8080", router)
+
+}
+
+type spaHandler struct {
+	staticFS   embed.FS
+	staticPath string
+	indexPath  string
+}
+
+func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// get the absolute path to prevent directory traversal
+	path := r.URL.Path
+	// if err != nil {
+	// 	// if we failed to get the absolute path respond with a 400 bad request and stop
+	// 	http.Error(w, err.Error(), http.StatusBadRequest)
+	// 	return
+	// }
+	// prepend the path with the path to the static directory
+	path = h.staticPath + path
+	fmt.Println(path)
+
+	_, err := h.staticFS.Open(path)
+	if os.IsNotExist(err) {
+		// file does not exist, serve index.html
+		fmt.Println(path)
+		index, err := h.staticFS.ReadFile(h.staticPath + "/" + h.indexPath)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusAccepted)
+		w.Write(index)
+		return
+	} else if err != nil {
+		// if we got an error (that wasn't that the file doesn't exist) stating the
+		// file, return a 500 internal server error and stop
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// get the subdirectory of the static dir
+	statics, err := fs.Sub(h.staticFS, h.staticPath)
+	// otherwise, use http.FileServer to serve the static dir
+	http.FileServer(http.FS(statics)).ServeHTTP(w, r)
 }
